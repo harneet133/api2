4d2c073fcdd7f79b53d71461ceb7ac0ef02948a3
Harneet Signh <83810684+harneet133@users.noreply.github.com>
2023-05-03 04:04:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
-@app.route('/get-rates', methods=['GET'])
-def get_rates():
-    # Get the currency conversion parameters from the query string
-    from_currency = request.args.get('from_currency')
-    to_currency = request.args.get('to_currency')
-    amount = request.args.get('amount')
+@app.route('/video-info', methods=['GET'])
+def get_video_info():
+    video_url = request.args.get('url')
+    if not video_url:
+        return jsonify({'error': 'Missing video URL parameter.'}), 400
 
-    # Customize the URL for the currency conversion
-    url = f'https://www.calculator.net/currency-calculator.html?eamount={amount}&efrom={from_currency}&eto={to_currency}&ccmajorccsettingbox=1&type=1&x=Calculate'
-
-    # Make a GET request to the URL and parse the HTML with Beautiful Soup
-    response = requests.get(url)
+    # Make a GET request to the YouTube video page and parse the HTML with Beautiful Soup.
+    response = requests.get(video_url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract the conversion rate from the paragraph tag with id "verybigtext"
-    rate = soup.find('p', {'class': 'verybigtext'}).text
+    # Extract the video title, description, upload date, view count, and thumbnail URL.
+    title = soup.find('meta', property='og:title')['content']
+    description = soup.find('meta', property='og:description')['content']
+    upload_date = soup.find('meta', itemprop='uploadDate')['content']
+    view_count = soup.find('meta', itemprop='interactionCount')['content']
+    thumbnail_url = soup.find('link', itemprop='thumbnailUrl')['href']
 
-    # Return the conversion rate as a JSON response
-    return render_template("index.html", rates=rate)
+    # Return the video information as a JSON response.
+    return jsonify({
+        'title': title,
+        'description': description,
+        'upload_date': upload_date,
+        'view_count': view_count,
+        'thumbnail_url': thumbnail_url
+    }), 200
 
 
 if __name__ == '__main__':
